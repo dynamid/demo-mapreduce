be12b159ec007638f81b0aa33d4719d609cc77ba
Olivier Nicolas <olivier.nicolas@insa-lyon.fr>
2023-08-01 11:32:25 +0100

    Changed output text file's name

--- 2-docker/orchestration/combine_files.py
+++ 2-docker/orchestration/combine_files.py
@@ -2,7 +2,7 @@ import glob
 
 read_files = glob.glob("data/reduced/*.txt")
 
-with open("data/final/final_text_freq.txt", "wb") as outfile:
+with open("mapreduce_docker_results.txt", "wb") as outfile:
     for f in read_files:
         with open(f, "rb") as infile:
             outfile.write(infile.read())
