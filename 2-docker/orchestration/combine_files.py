ec9d53f548d33f1818906cce86856ee010ba549d
Olivier Nicolas <olivier.nicolas@insa-lyon.fr>
2023-09-08 10:28:30 +0100

    Minor changes in result path/loation for k3s version

--- 2-docker/orchestration/combine_files.py
+++ 2-docker/orchestration/combine_files.py
@@ -2,7 +2,7 @@ import glob
 
 read_files = glob.glob("data/reduced/*.txt")
 
-with open("mapreduce_docker_results.txt", "wb") as outfile:
+with open("../results/mapreduce_docker_results.txt", "wb") as outfile:
     for f in read_files:
         with open(f, "rb") as infile:
             outfile.write(infile.read())
