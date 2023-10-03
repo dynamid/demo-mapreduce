fc673e0f6a36a3eadd08ba8bf8c550e4e1751fff
olivier <olivier.nicolas@insa-lyon.fr>
2023-10-03 09:57:57 +0200

    Adding function definition and calls

--- 2-docker/mkimgs.sh
+++ 2-docker/mkimgs.sh
@@ -1,31 +1,20 @@
 #!/bin/sh
 
+repo=dhub.ox
+
+build_push () {
+  cd $1
+    docker build --no-cache -t $repo/$1 .
+    docker push $repo/$1
+#  cd ..
+}
+
 echo "--------------------------------- IMAGE CREATION START ------------------------\n\n"
 
-#build python3-alpine image
-cd python3.12-rc-alpine
-  docker build --no-cache -t dhub.ox/python3:3.12-rc-alpine .
-  docker push dhub.ox/python3:3.12-rc-alpine
-cd ..
-
-# Build Map Script Image
-cd map
-  docker build --no-cache -t dhub.ox/map .
-  docker push dhub.ox/map
-cd ..
-
-# Build Combine Script Image
-cd combine
-  docker build --no-cache -t dhub.ox/combine .
-  docker push dhub.ox/combine
-cd ..
-
-# Build Reduce Script Image
-
-cd reduce
-  docker build --no-cache -t dhub.ox/reduce .
-  docker push dhub.ox/reduce
-cd ..
+build_push python3:3.12-rc-alpine
+build_push map
+build_push combine
+build_push reduce
 
 # Remove unused images
 docker image prune -f
