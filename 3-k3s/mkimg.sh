f0e2501913c1018e1c962e94c354a76faee3201e
olivier <olivier.nicolas@insa-lyon.fr>
2023-10-12 17:14:48 +0200

    Adding the possibility to use the --no-cache option

--- 3-k3s/mkimg.sh
+++ 3-k3s/mkimg.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+# option: we would use "--no-cache"
+option=$2
 repo=dhub.ox/simple-mapreduce
 
-#docker build --no-cache -t dhub.ox/simple-mapreduce .
-docker build -t $repo .
+docker build $option -t $repo .
 docker push $repo
